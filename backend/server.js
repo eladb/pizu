4582074133e8f3d627b066f675b1ebc93c2d436a
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-05-21 22:28:55 +0300

    add githubhook

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 var server = require('./lib/app');
 server.listen(process.env.PORT || 3000);
 console.log('Listening on %d', process.env.PORT || 3000);
+
+// create hook for github deployment
+var githubhook = require('githubhook');
+githubhook(8123, { 'pizuDeploy1980': 'https://github.com/eladb/pizu' }, function (err, payload) {
+  if (err) {
+    console.warn(err);
+    return;
+  }
+  else {
+    console.log('github post-recieve hook - exiting');
+    process.exit(1);
+  }
+});
+console.log('Started github hook on 8123');
