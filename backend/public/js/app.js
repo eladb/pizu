878d60776b482d0942e940f106c385c8767c9777
unknown <Shai@Shai-Laptop.(none)>
2012-05-22 16:01:04 +0300

    Playing with adding GeoLocation support

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -60,6 +60,49 @@ $(function() {
     var name = $('#name').attr('value');
     var cid = $('#cid').attr('value');
 
+
+    if (navigator.geolocation) 
+    {
+        navigator.geolocation.getCurrentPosition( 
+ 
+        function (position) {  
+ 
+        // Did we get the position correctly?
+        alert ('latitude: ' + position.coords.latitude + ' , longitude: ' + position.coords.longitude);
+        console.log(position);
+
+
+        //To see everything available in the position.coords array:
+        //for (key in position.coords) {alert(key)}
+     
+        //mapServiceProvider(position.coords.latitude,position.coords.longitude);
+     
+        }, 
+        // next function is the error callback
+        function (error)
+        {
+          switch(error.code) 
+          {
+            case error.TIMEOUT:
+              alert ('Timeout');
+              break;
+            case error.POSITION_UNAVAILABLE:
+              alert ('Position unavailable');
+              break;
+            case error.PERMISSION_DENIED:
+              alert ('Permission denied');
+              break;
+            case error.UNKNOWN_ERROR:
+              alert ('Unknown error');
+              break;
+          }
+        }
+        );
+      }
+    //}
+    //else // finish the error checking if the client is not compliant with the spec
+
+
     console.log('name=%s cid=%d', name, cid);
 
     var url = '/?sid=1&cid=' + cid;
