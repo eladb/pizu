d94371352afae34374c3534b0493d4fd6646ef82
ramg <ramgreenberg@hotmail.com>
2012-06-04 16:47:37 +0300

    accelerometer

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -52,6 +52,40 @@ $(function() {
     window.location.reload();
   });
 
+   // Start watching the acceleration
+   //
+   function startWatch() {
+       // Update acceleration every 3 seconds
+       var options = { frequency: 2000 };
+
+       watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+   }
+
+   // Stop watching the acceleration
+   //
+   function stopWatch() {
+       if (watchID) {
+           navigator.accelerometer.clearWatch(watchID);
+           watchID = null;
+       }
+   }
+
+    // acceleration onSuccess
+    //
+    function onSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />');
+
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
+
 
   function pair() {
     if (!cid || !name) {
@@ -218,6 +252,8 @@ $(function() {
     });
   }
 
+  startWatch();
+  
   var refreshImage = new Image();
   refreshImage.src = 'img/refresh.png';
   var refreshButton = null;
