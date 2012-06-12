3584b0513eb8b02acf8a09b7befdae9166eeb9ca
unknown <ram@ram_desktop.(none)>
2012-06-12 17:19:03 +0300

    ccs3 animation

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -7,10 +7,11 @@
 
 var APP_ID = '146577522141106';
 
-$(function() {
 
   var cid = null;
   var name = null;
+
+$(function() {
 
   function after_login() {
     FB.api('/me', function(response) {
@@ -54,127 +55,129 @@ $(function() {
       }
     });
   }, false);
+});
 
-  function shakeAnimation() {
-    console.log("shake it shake it baby ...");
-    var img = document.getElementById('shakeIphone');
-    img.className = 'shake';
-    img.addEventListener('webkitAnimationEnd', function(){
-      this.className = '';
-    });
+function shakeAnimation() {
+  console.log("shake it shake it baby ...");
+  var img = document.getElementById('shakeIphone');
+  img.className = 'shake';
+  img.addEventListener('webkitAnimationEnd', function(){
+    this.className = '';
+  });
 
-    //$('div#shake img').removeClass("");
-    //console.log($('div#shake img'));
-    
-    //$('div#shake img').addClass("shake");
-    //console.log($('div#shake img'));
+  //$('div#shake img').removeClass("");
+  //console.log($('div#shake img'));
+  
+  //$('div#shake img').addClass("shake");
+  //console.log($('div#shake img'));
 
-    //$('div#shake img').transition({ rotate: '30deg' },{queue:true,duration:'slow',easing:"easein"});
-    //$('div#shake img').transition({ rotate: '-30deg' },{queue:true,duration:'slow',easing:"easein"});
-    //$('div#shake img').transition({ rotate: '30deg' },{queue:true,duration:'slow',easing:"easein"});
-    //$('div#shake img').transition({ rotate: '-30deg' },{queue:true,duration:'slow',easing:"easein"});
-    //$('div#shake img').animate({rotate: '100'},{duration : 1000});
-    //$('div#shake img').css("-webkit-animation", "shake 0.7s ease-in-out 0s 4 alternate");
+  //$('div#shake img').transition({ rotate: '30deg' },{queue:true,duration:'slow',easing:"easein"});
+  //$('div#shake img').transition({ rotate: '-30deg' },{queue:true,duration:'slow',easing:"easein"});
+  //$('div#shake img').transition({ rotate: '30deg' },{queue:true,duration:'slow',easing:"easein"});
+  //$('div#shake img').transition({ rotate: '-30deg' },{queue:true,duration:'slow',easing:"easein"});
+  //$('div#shake img').animate({rotate: '100'},{duration : 1000});
+  //$('div#shake img').css("-webkit-animation", "shake 0.7s ease-in-out 0s 4 alternate");
+}
+
+// The watch id references the current `watchAcceleration`
+var watchID = null;
+  
+// Start watching the acceleration
+function startWatch() {
+  
+  console.log('visible');
+  $('div#shake').css("visibility","visible");
+  shakeAnimation();
+
+  if (!('accelerometer' in navigator)) {
+    console.log('no accelerometer');
+    return;
   }
 
-  // The watch id references the current `watchAcceleration`
-  var watchID = null;
-    
-  // Start watching the acceleration
-  function startWatch() {
-    
-    if (!('accelerometer' in navigator)) {
-      console.log('no accelerometer');
-      return;
-    }
+  var previousReading = {
+      x: null,
+      y: null,
+      z: null
+  }
 
-    var previousReading = {
-        x: null,
-        y: null,
-        z: null
-    }
-
-    console.log('visible');
-    $('div#shake').css("visibility","visible");
-    shakeAnimation();
+  watchID = navigator.accelerometer.watchAcceleration(function (acceleration) {
+    var changes = {};
+    bound =10;
     
-    watchID = navigator.accelerometer.watchAcceleration(function (acceleration) {
-      var changes = {};
-      bound =10;
-      
-      if (previousReading.x !== null) {
-        changes.x = Math.abs(previousReading.x - acceleration.x);
-        changes.y = Math.abs(previousReading.y - acceleration.y);
-        changes.z = Math.abs(previousReading.z - acceleration.z);
+    if (previousReading.x !== null) {
+      changes.x = Math.abs(previousReading.x - acceleration.x);
+      changes.y = Math.abs(previousReading.y - acceleration.y);
+      changes.z = Math.abs(previousReading.z - acceleration.z);
 
-        if (changes.x > bound && changes.y > bound) {
-          shaken();
-        }
+      if (changes.x > bound && changes.y > bound) {
+        shaken();
       }
-        
-      previousReading = {
-        x: acceleration.x,
-        y: acceleration.y,
-        z: acceleration.z
-      }        
-    }, onAccelerometerError, { frequency: 100 });
-  }
-
-  function shaken(){
-    $('div#shake').css("visibility","hidden");
-    navigator.accelerometer.clearWatch(watchID);
-    navigator.notification.vibrate(2500);
-    pair();
-  }
-
-  // Error
-  function onAccelerometerError() {
-    alert("Acceleration Error occured");
-  }
-
-  function pair() {
-    if (!cid || !name) {
-      console.error('still not logged in');
-      return;
     }
+      
+    previousReading = {
+      x: acceleration.x,
+      y: acceleration.y,
+      z: acceleration.z
+    }        
+  }, onAccelerometerError, { frequency: 100 });
+}
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {  
-        console.log(position);
+function shaken(){
+  $('div#shake').css("visibility","hidden");
+  navigator.accelerometer.clearWatch(watchID);
+  navigator.notification.vibrate(2500);
+  pair();
+}
 
-        var geoHash = encodeGeoHash(position.coords.latitude, position.coords.longitude);
-        console.log('geoHash: ' + geoHash);
+// Error
+function onAccelerometerError() {
+  alert("Acceleration Error occured");
+}
 
-        geoHash = geohash.slice(0,7);
+function pair() {
+  console.log("Pair");
+  if (!cid || !name) {
+    console.error('still not logged in');
+    return;
+  }
 
-        console.log('geoHash after slice: ' + geoHash);
-        console.log('name=%s cid=%d', name, cid);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {  
+      console.log(position);
 
-        var url = '/?sid=' + geoHash + '&cid=' + cid;
+      var geoHash = encodeGeoHash(position.coords.latitude, position.coords.longitude);
+      console.log('geoHash: ' + geoHash);
 
-        console.log('sending post request', url);
+      geoHash = geohash.slice(0,7);
 
-        $.ajax({
-          type: 'post',
-          url: url,
-          data: { name: name, fbid: cid },
-        }).done(function(payload) {
-          console.log('payload:' + payload);
-          
-          for (var k in payload) {
-            console.log('k=', k);
-            if (k != cid) {
-              var other = payload[k];
+      console.log('geoHash after slice: ' + geoHash);
+      console.log('name=%s cid=%d', name, cid);
 
-              console.log('found other payload:', payload[k]);
+      var url = '/?sid=' + geoHash + '&cid=' + cid;
 
-              var fbid = other.fbid;
+      console.log('sending post request', url);
 
-              var graph = '/me/mutualfriends/' + fbid;
+      $.ajax({
+        type: 'post',
+        url: url,
+        data: { name: name, fbid: cid },
+      }).done(function(payload) {
+        console.log('payload:' + payload);
+        
+        for (var k in payload) {
+          console.log('k=', k);
+          if (k != cid) {
+            var other = payload[k];
 
-              FB.api(graph, function(res) {
-                console.log('response:'+JSON.stringify(res));
-                var friends = res.data;
+            console.log('found other payload:', payload[k]);
+
+            var fbid = other.fbid;
+
+            var graph = '/me/mutualfriends/' + fbid;
+
+            FB.api(graph, function(res) {
+              console.log('response:'+JSON.stringify(res));
+              var friends = res.data;
 //                //showFriends(friends);
 //                //stop backround animation
 //                clearInterval(backroundIntervalId);
@@ -195,51 +198,56 @@ $(function() {
 //                    sharedFriends.push(img);
 //                  }; 
 //                };
-              });
-            }
+            });
           }
-        }).fail(function(jqxhr, textStatus, body) {
-          alert(body);
-          startWatch();
-        });
-      }, function (error) {
-        startWatch();
-        switch(error.code) 
-        {
-          case error.TIMEOUT:
-            alert ('Timeout');
-            break;
-          case error.POSITION_UNAVAILABLE:
-            alert ('Position unavailable');
-            break;
-          case error.PERMISSION_DENIED:
-            alert ('Permission denied');
-            break;
-          case error.UNKNOWN_ERROR:
-            alert ('Unknown error');
-            break;
         }
+      }).fail(function(jqxhr, textStatus, body) {
+        alert(body);
+        startWatch();
       });
-    }
-    else{
+    }, function (error) {
       startWatch();
-    }
+      switch(error.code) 
+      {
+        case error.TIMEOUT:
+          alert ('Timeout');
+          break;
+        case error.POSITION_UNAVAILABLE:
+          alert ('Position unavailable');
+          break;
+        case error.PERMISSION_DENIED:
+          alert ('Permission denied');
+          break;
+        case error.UNKNOWN_ERROR:
+          alert ('Unknown error');
+          break;
+      }
+    });
   }
+  else{
+    startWatch();
+  }
+}
 
-  function loadFriendsImages(friends){
-    return friends.forEach(function(friend) {
-        var imageURL = 'https://graph.facebook.com/' + friend.id + '/picture';
-        var img = new Image();
-        img.src = imageURL;
+function loadFriendsImages(friends){
+  return friends.forEach(function(friend) {
+    var imageURL = 'https://graph.facebook.com/' + friend.id + '/picture';
+    var img = new Image();
+    img.src = imageURL;
 
-        img.onload = function() {
+    index = 0;
+    img.onload = function() {
+      if(index < 20)
+      {
         var firstName=friend.name.split(" ")[0];
         //var line = "<li><a href=\"" + imageURL + "\" title=\""+ firstName +"\">"+img.outerHTML+"</img></a></li>";
         var line = "<li><a href=\"#\" title=\""+ firstName +"\">"+img.outerHTML+"</img></a></li>";
         $('ul').append(line);
+      }
+      index++;
     };
   });
- }
-});
+}
+
 
 
