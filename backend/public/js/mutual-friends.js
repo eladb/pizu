54a2b2e101932a6e6f9abfcb6bf1814e202e8d06
unknown <Shai@Shai-Laptop.(none)>
2012-06-22 21:15:20 +0300

    fixed mf page to fit iphone

--- backend/public/js/mutual-friends.js
+++ backend/public/js/mutual-friends.js
@@ -52,13 +52,9 @@ $(function () {
             });
 
             $('#slider').movingBoxes({
-                /* width and panelWidth options deprecated, but still work to keep the plugin backwards compatible
-                width: 500,
-                panelWidth: 0.5,
-                */
                 startPanel: 1,      // start with this panel
                 wrap: false,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
-                buildNav: true,   // if true, navigation links will be added
+                buildNav: false,   // if true, navigation links will be added
                 navFormatter: function () { return "&#9679;"; } // function which returns the navigation text for each panel
             });
 
