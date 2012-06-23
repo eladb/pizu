4fc10103fccffa7f1d514dbd6ab5d7f6dbf9775e
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-06-24 02:57:51 +0300

    Update master

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 var server = require('./lib/app');
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || process.env.port || 3000);
 console.log('Listening on %d', process.env.PORT || process.env.port || 3000);
 
 // create hook for github deployment
